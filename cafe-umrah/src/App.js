c7d4d797a472fcf51f368019038b8e5ea65ebf02
fahri-ts <gamedemodownload@gmail.com>
2022-02-09 20:45:28 +0700

    coba git bash untuk update repo.

--- cafe-umrah/src/App.js
+++ cafe-umrah/src/App.js
@@ -1,17 +1,15 @@
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <div className="App-header">
-        <h1>Header1</h1>
-		<h2>Header2</h2>
-		<h3>Header3</h3>
-		<h4>Header4</h4>
-		<h5>Header5</h5>
-		<h6>Header6</h6>
-		<p>Paragrap</p>
-      </div>
+      <header className="App-header">
+		<h1>Header</h1>
+      </header>
+	  <body className="App-body">
+		<h3>Body</h3>
+	  </body>
     </div>
   );
 }
